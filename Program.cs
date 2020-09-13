a1782bbf4f39f0b833e1391f8175c87a52317b45
Felipe França <contatofelipefranca12@gmail.com>
2020-09-13 18:10:35 -0300

    Adiciona Cidades

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("----------Cidades do Litoral Sul de SP----------");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Cananéia");
+            Console.ReadKey();
+        }
+    }
+}
